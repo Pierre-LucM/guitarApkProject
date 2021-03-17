a6815bfb587070f295a6d08d13d34d54d257096a
Pierre-Luc Millet <pierre-luc.millet@student.yncrea.fr>
2021-03-17 09:09:45 +0100

    adding new page and button

--- app/src/main/java/com/example/guitarplayer/FirstFragment.java
+++ app/src/main/java/com/example/guitarplayer/FirstFragment.java
@@ -1,5 +1,6 @@
 package com.example.guitarplayer;
 
+import android.net.wifi.rtt.WifiRttManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,12 +24,22 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
+        view.findViewById(R.id.search_nearby_device).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+                        .navigate(R.id.action_FirstFragment_to_searchBluetoothDevice);
             }
         });
+
+      /* view.findViewById(R.id.search_nearby_device).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(FirstFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_SearchBluetoothDevice);
+            }
+        });*/
     }
+
+
 }
